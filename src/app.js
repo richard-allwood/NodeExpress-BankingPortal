a16effbb6e8b61f870e3a9011cf96b0969056089
Richard Allwood <richard.m.allwood@gmail.com>
2018-08-29 22:30:06 +1000

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -9,6 +9,17 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+const readJson = filename => {
+  const text = fs.readFileSync(path.join(__dirname, 'json', filename), 'utf8');
+  return JSON.parse(text);
+};
+const accounts = readJson('accounts.json');
+const users = readJson('users.json');
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => console.log('PS Project running on port 3000!'));
