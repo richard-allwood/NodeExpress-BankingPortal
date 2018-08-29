2ac6adc01ea7fc1d72a3d5fb246513e0fa950b2f
Richard Allwood <richard.m.allwood@gmail.com>
2018-08-29 22:46:46 +1000

    Handling Form Data

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const readJson = filename => {
   const text = fs.readFileSync(path.join(__dirname, 'json', filename), 'utf8');
@@ -16,10 +17,32 @@ const readJson = filename => {
 const accounts = readJson('accounts.json');
 const users = readJson('users.json');
 
+const saveAccounts = () => {
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+};
+
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+  saveAccounts();
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+  saveAccounts();
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => console.log('PS Project running on port 3000!'));
